b5cfc1e962bb58ef161d71b824852127ba39e49c
Abdulla Farooq <voilad8a@gmail.com>
2023-08-12 13:37:53 +0300

    Changes to be committed: 	modified:   src/Error.jsx

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
-  return <div></div>;
+  return (
+    <section
+      className="w-full h-screen flex items-center justify-center"
+      id="About"
+    >
+      <div className="flex flex-col text-4xl gap-2">
+        404 | ERROR
+        <Link to={"/"} className="p-2 text-center bg-accent">
+          Go Back
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Error;
