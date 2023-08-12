5f5c3a6891be0814fa1d2fccd2084018ca6c63e8
Abdulla Farooq <voilad8a@gmail.com>
2023-08-12 13:29:54 +0300

    Changes to be committed: 	modified:   src/About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -1,17 +1,15 @@
 const About = () => {
   return (
     <section className="mx-auto animate-fade ease-in-out py-20 p-6" id="About">
-      <div className="flex sm:flex-row flex-col max-w-7xl bg-accent text-black mx-auto p-6 rounded-md shadow-md">
+      <div className="flex sm:flex-row flex-col max-w-7xl bg-secondary text-white mx-auto p-6 rounded-md shadow-md">
         <img
           src="Images/AboutImage.jpg"
           className="drop-shadow-md h-96 object-cover"
         />
 
-        <div className="my-auto p-3 flex flex-col justify-center">
-          <p className="text-5xl font-semibold text-center sm:text-justify">
-            Who Are We?
-          </p>
-          <p className="text-xl text-justify text-gray-600">
+        <div className="my-auto p-3 flex flex-col gap-16 justify-center">
+          <p className="text-5xl font-semibold text-center">Who Are We?</p>
+          <p className="text-xl text-justify">
             At Plant Palace Nursery, we are passionate about all things green
             and growing. Our journey began with a simple idea: to create a haven
             for plant enthusiasts, both seasoned gardeners and budding green
