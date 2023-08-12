54204632327a2fbe0b920d8d566d889f1c5c3c3b
Abdulla Farooq <voilad8a@gmail.com>
2023-08-12 10:02:17 +0300

    modified:   index.html 	modified:   src/Gallery.jsx

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,10 +2,10 @@ const Gallery = () => {
   return (
     <section className="max-w-7xl mx-auto">
       <h2 className="text-3xl text-center mb-4">Browse Our Gallery</h2>
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-2">
-        <img src="Images/Gallery/G1.jpg" alt="" />
-        <img src="Images/Gallery/G2.jpg" alt="" />
-        <img src="Images/Gallery/G3.jpg" alt="" />
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-2 animate-fade ease-in-out">
+        <img src="Images/Gallery/G1.jpg" loading="lazy" alt="" />
+        <img src="Images/Gallery/G2.jpg" loading="lazy" alt="" />
+        <img src="Images/Gallery/G3.jpg" loading="lazy" alt="" />
       </div>
     </section>
   );
